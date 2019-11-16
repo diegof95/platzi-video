f7331e435d0f7ec192ef9fc2245a64a46dddbe24
diegof95 <diego.botero@correounivalle.edu.co>
2019-11-16 00:23:43 -0500

    Agregado componente de vista RelatedLayout, representa la columna izquierda del sitio donde se muestran listas de multimedia relacionadas a nuestro usuario.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import HomeLayout from './home-layout';
 import Feed from '../feed/feed';
+import RelatedLayout from '../related/related-layout';
 
 class Home extends Component {
 
   render(){
     return(
       <HomeLayout>
+        <RelatedLayout />
         <Feed data={this.props.data} />
       </HomeLayout>
     )
---
+++ src/related/related-layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './related.css'
+import logo from '../../images/logo.png';
+
+function RelatedLayout(props) {
+
+  return(
+    <div>
+      <img className='Related' src={logo} alt='Logo' width={250} />
+    </div>
+  )
+}
+
+export default RelatedLayout;
